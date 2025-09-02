fc6661e1e12bd419efa4d1157f45b4a68dcd9d34
PhilipBirkeland <157786777+PhilipBirkeland@users.noreply.github.com>
2025-09-02 09:14:13 +0200

    tilføjet puls
    tilføjet puls til firkant

--- sketch.js
+++ sketch.js
@@ -1,9 +1,20 @@
+let square_size = 50;
+let ms;
+
 function setup() {
   createCanvas(400, 400);
   rectMode(CENTER);
+  frameRate(60);
 }
 
 function draw() {
+  ms = millis();
   background(220);
-  rect(width / 2, height / 2, 50, 50);
+  rect(width / 2, height / 2, square_size, square_size);
+  if (square_size > 50) {
+    square_size -= 4;
+  }
+  if (frameCount % 60 == 0 || frameCount % 60 == 10) {
+    square_size += 25;
+  }
 }
